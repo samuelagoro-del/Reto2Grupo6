febe9e40a0c492df91e50872a524b450c7a880ae
samuelagoro-del <samuel.agoro@elorrieta-errekamari.com>
2026-02-05 13:03:42 +0100

    Refactor Compra class by removing redundant code

--- Compra.java
+++ Compra.java
@@ -4,7 +4,6 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 
 public class Compra {
-<<<<<<< HEAD
     private int idCompra; // int porque es auto_increment en la BD
     private LocalDate fecha;
     private LocalTime hora;
@@ -40,93 +39,4 @@ public class Compra {
 
     public Cliente getCliente() { return cliente; }
     public void setCliente(Cliente cliente) { this.cliente = cliente; }
-=======
-	private String idcompra;
-	private LocalDate fecha;
-	private LocalTime hora;
-	private double precio;
-	private float descuento;
-	private Cliente cliente;
-	
-	
-	public Compra(String idcompra, LocalDate fecha, LocalTime hora, double precio, float descuento, Cliente cliente) {
-		this.idcompra = idcompra;
-		this.fecha = fecha;
-		this.hora = hora;
-		this.precio = precio;
-		this.descuento = descuento;
-		this.cliente = new Cliente();
-	}
-
-	public Compra() {
-		this.idcompra = "";
-		this.fecha = "";
-		this.hora = "";
-		this.precio = 0;
-		this.descuento = 0;
-		this.cliente = new Cliente();
-	}
-
-
-	
-	public String getIdcompra() {
-		return idcompra;
-	}
-
-
-	public void setIdcompra(String idcompra) {
-		this.idcompra = idcompra;
-	}
-
-
-	public LocalDate getFecha() {
-		return fecha;
-	}
-
-
-	public void setFecha(LocalDate fecha) {
-		this.fecha = fecha;
-	}
-
-
-	public LocalTime getHora() {
-		return hora;
-	}
-
-
-	public void setHora(LocalTime hora) {
-		this.hora = hora;
-	}
-
-
-	public double getPrecio() {
-		return precio;
-	}
-
-
-	public void setPrecio(double precio) {
-		this.precio = precio;
-	}
-
-
-	public float getDescuento() {
-		return descuento;
-	}
-
-
-	public void setDescuento(float descuento) {
-		this.descuento = descuento;
-	}
-
-
-	public Cliente getCliente() {
-		return cliente;
-	}
-
-
-	public void setCliente(Cliente cliente) {
-		this.cliente = new Cliente();
-	}
- 
->>>>>>> 31630d8dfc56230ee0381335234046ff98a0e21c
 }
